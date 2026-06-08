8c6435be2d68a14b85fa95fd64862dfd86f7f31e
lomaski <lomaski1986@gmail.com>
2026-06-08 17:36:34 -0300

    modified:   index.html 	new file:   scripts.js

---
+++ scripts.js
@@ -0,0 +1,16 @@
+const form = document.querySelector('.fale-conosco')
+const background = document.querySelector('.mascara-form')
+
+    function showForm() {
+        form.style.left = '50%'
+        form.style.transform = 'translateX(-50%)'
+        background.style.visibility = 'visible'
+    }
+
+    function hideForm() {
+        form.style.left = '-330px'
+        form.style.transform = 'translateX(0)'
+        background.style.visibility = 'hidden'
+    }
+
+    
